7871971a5cbe3cb20e55c42e31aebcf46598b795
taimuraq <1750380+taimuraq@users.noreply.github.com>
2025-04-24 03:44:50 +0500

    fixing yaml

--- src/main/java/com/example/userdataapi/controller/CompanySettingsController.java
+++ src/main/java/com/example/userdataapi/controller/CompanySettingsController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/companysettings/v1")
+@RequestMapping("/companysettings")
 @RequiredArgsConstructor
 @Tag(name = "Company Settings")
 public class CompanySettingsController {
