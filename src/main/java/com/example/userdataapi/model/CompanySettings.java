b48533205112261ce830c2fa891d68a62083a6c5
taimuraq <1750380+taimuraq@users.noreply.github.com>
2025-04-24 00:25:46 +0500

    adding new controller

---
+++ src/main/java/com/example/userdataapi/model/CompanySettings.java
@@ -0,0 +1,11 @@
+package com.example.userdataapi.model;
+
+import lombok.Data;
+
+@Data
+public class CompanySettings {
+  private String unitId;
+  private String settingName;
+  private String settingValue;
+}
+
